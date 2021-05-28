f35b8147c92dd7c82d1c086b300e95504ca30fa3
Neko Alosama <47763383+NekoAlosama@users.noreply.github.com>
2021-05-28 09:05:37 -0400

    Update difference.rs

--- src/difference.rs
+++ src/difference.rs
@@ -12,21 +12,20 @@ Excluded formulas:
  - Euclidian distance for all axes
    - Does not work well for Oklab.L distances
 */
+#![allow(dead_code)]
 
 extern crate oklab;
 use oklab::*;
 
-#[allow(dead_code)]
 pub fn hyab(lab: &Oklab, lab2: &Oklab) -> f32 {
     /*
     HyAB (Hybrid of absolute value and Euclidian distance): |L_1 - L_2| + dist((a_1, b_1), (a_2, b_2))
     An alternative to CIEDE2000 since it doesn't work with differences >10 and this is simpler
     Originally used with CIELAB inputs as it was created before Oklab (http://markfairchild.org/PDFs/PAP40.pdf)
     Color generated: (109, 110, 66) */
-    (lab.l - lab2.l).abs() + ((lab.a - lab2.a).powi(2) + (lab.b - lab2.b).powi(2)).sqrt()
+    (lab.l - lab2.l).abs() + (lab.a - lab2.a).hypot(lab.b - lab2.b)
 }
 
-#[allow(dead_code)]
 pub fn absolute(lab: &Oklab, lab2: &Oklab) -> f32 {
     /*
     Absolute value: |L_1 - L_2| + |a_1 - a_2| + |b_1 - b_2|
